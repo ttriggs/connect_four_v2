dab40020826d8416a7119cb04008735dfb25334a
ttriggs <tyler.triggs@gmail.com>
2015-08-13 13:11:21 -0500

    remove window arg in image call

--- lib/background.rb
+++ lib/background.rb
@@ -2,7 +2,7 @@ class Background
   def initialize(window)
     @window = window
     bg = ['bg1.png', 'bg2.png', 'bg3.png'].sample
-    @bg_image = Gosu::Image.new(window, "img/backgrounds/#{bg}")
+    @bg_image = Gosu::Image.new("img/backgrounds/#{bg}")
   end
 
   def draw
